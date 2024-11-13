f8cce40f3af8ec8484071b04f1757970946647f9
Jannatul-2003 <jannatul19112003@gmail.com>
2024-11-13 19:07:18 +0600

    Added firebase and signUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
-import 'package:momento/controller/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:momento/screen/auth.dart';
 import 'package:momento/view/themedata.dart';
 import 'package:momento/view/dark_themedata.dart';
-void main(){
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
     theme: themeData,
     darkTheme: darkThemeData,
     themeMode: ThemeMode.system,
     debugShowCheckedModeBanner: false,
-    home:const Expenses(),
+    home:const AuthScreen(),
   ),);
 }
---
+++ lib/screen/auth.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:momento/controller/expenses.dart';
+import 'package:momento/view/password_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _enteredPassword = "";
+  var _enteredEmail = "";
+
+  void _submit() async {
+    final isValid = _formKey.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    _formKey.currentState!.save();
+    if (_isLogin) {
+      // Log in
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => Expenses()));
+    } else {
+      try {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+      } on FirebaseAuthException catch (error) {
+        var message = 'An error occurred, please check your credentials!';
+        if (error.message != null) {
+          message = error.message!;
+        }
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(message),
+            backgroundColor: Theme.of(context).colorScheme.error,
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.fromLTRB(20, 30, 20, 20),
+                width: 200,
+                child: const Text("Hello, Welcome to Momento!"),
+              ),
+              Card(
+                margin: const EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: 'Email Address'),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            validator: (value) {
+                              String? errorText = validateEmailStructure(value);
+                              return errorText;
+                            },
+                            onSaved: (newValue) => _enteredEmail = newValue!,
+                          ),
+                          Flexible(
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'Password',
+                                isDense: true,
+                                errorMaxLines: 2,
+                              ),
+                              obscureText: true,
+                              validator: (value) {
+                                String? errorText =
+                                    validatePasswordStructure(value);
+                                return errorText;
+                              },
+                              onSaved: (newValue) =>
+                                  _enteredPassword = newValue!,
+                            ),
+                          ),
+                          const SizedBox(height: 12),
+                          ElevatedButton(
+                            onPressed: _submit,
+                            child: Text(_isLogin ? 'Log in' : 'Sign Up'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(_isLogin
+                                ? "Create an account"
+                                : "I already have an account."),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
