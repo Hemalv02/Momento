c58cb2961ce51c2037400ba6798e345b722f69cd
Jannatul-2003 <jannatul19112003@gmail.com>
2025-01-27 22:16:23 +0600

    Added deletion of coorganizer

--- lib/screens/events/event_co_organizer.dart
+++ lib/screens/events/event_co_organizer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:momento/main.dart';
 import 'package:momento/screens/events/co_organizer_add.dart';
 import 'package:momento/screens/events/fetch_organizer_bloc/fetch_organizer_bloc.dart';
@@ -53,7 +52,8 @@ class _EventCoOrganizerState extends State<EventCoOrganizer> {
           .eq('username', username)
           .single();
 
-      if (response != null && response['url'] != null) {
+      // if (response != null && response['url'] != null) {
+      if (response['url'] != null) {
         final url = response['url'];
         final imageResponse = await http.get(Uri.parse(url));
 
@@ -90,6 +90,7 @@ class _EventCoOrganizerState extends State<EventCoOrganizer> {
           backgroundColor: const Color(0xFF003675),
           child: const Icon(Icons.add),
         ),
+        backgroundColor: Colors.white,
         body: RefreshIndicator(
           onRefresh: _onRefresh,
           child: BlocBuilder<FetchCoorganizerBloc, FetchCoorganizerState>(
@@ -122,6 +123,7 @@ class _EventCoOrganizerState extends State<EventCoOrganizer> {
                         return CoorganizerCard(
                           coorganizer: coorganizer,
                           profilePic: profilePic,
+                          eventId: widget.eventId,
                         );
                       },
                     );
@@ -155,23 +157,129 @@ class _EventCoOrganizerState extends State<EventCoOrganizer> {
 class CoorganizerCard extends StatelessWidget {
   final Coorganizer coorganizer;
   final Uint8List? profilePic;
+  final int eventId;
 
   const CoorganizerCard({
     super.key,
     required this.coorganizer,
     required this.profilePic,
+    required this.eventId,
   });
+
+  Future<void> deleteCoorganizer(String username, BuildContext context) async {
+    final supabase = Supabase.instance.client;
+
+    try {
+      // Fetch the user ID of the co-organizer based on the username
+      final response = await supabase
+          .from('users')
+          .select('id')
+          .eq('username', username)
+          .single();
+
+//     if (response == null || response['id'] == null) {
+      if (response['id'] == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('User with username $username not found.'),
+          ),
+        );
+        return;
+      }
+
+      final coorganizerId = response['id'];
+
+      // Delete the co-organizer for the given event
+      await supabase
+          .from('event_coorganizers')
+          .delete()
+          .eq('event_id', eventId)
+          .eq('user_id', coorganizerId);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Successfully removed $username as co-organizer.'),
+        ),
+      );
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to remove $username as co-organizer. $error'),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
+    return Dismissible(
+      key: Key(coorganizer.username), // Unique key for each dismissible widget
+      direction: DismissDirection.startToEnd, // Swipe from right to left
+      background: Container(
+        color: const Color(0xFF003675),
+        alignment: Alignment.centerLeft,
+        padding: const EdgeInsets.only(left: 20),
+        child: const Icon(Icons.delete, color: Colors.white),
       ),
-      color: const Color.fromARGB(255, 240, 246, 252),
-      child: ListTile(
-        leading: GestureDetector(
+      secondaryBackground: Container(
+        color: const Color(0xFF003675),
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        child: const Icon(Icons.delete, color: Colors.white),
+      ),
+      onDismissed: (direction) async {
+        // Perform deletion logic here
+        await deleteCoorganizer(coorganizer.username, context);
+      },
+      confirmDismiss: (direction) async {
+        return showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Confirm Deletion'),
+            content: Text(
+                'Are you sure you want to remove ${coorganizer.username}?'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: const Text('Delete'),
+              ),
+            ],
+          ),
+        );
+      },
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        color: const Color.fromARGB(255, 240, 246, 252),
+        child: ListTile(
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => UserProfileViewPage(
+                    viewedUsername: coorganizer.username,
+                  ),
+                ),
+              );
+            },
+            child: CircleAvatar(
+              radius: 25,
+              backgroundColor: profilePic == null
+                  ? const Color(0xFF003675)
+                  : Colors.transparent,
+              foregroundColor: Colors.white,
+              backgroundImage:
+                  profilePic != null ? MemoryImage(profilePic!) : null,
+              child: profilePic == null ? Text(coorganizer.username[0]) : null,
+            ),
+          ),
           onTap: () {
             Navigator.push(
               context,
@@ -182,39 +290,19 @@ class CoorganizerCard extends StatelessWidget {
               ),
             );
           },
-          child: CircleAvatar(
-            radius: 25.r,
-            backgroundColor: profilePic == null
-                ? const Color(0xFF003675)
-                : Colors.transparent,
-            foregroundColor: Colors.white,
-            backgroundImage:
-                profilePic != null ? MemoryImage(profilePic!) : null,
-            child: profilePic == null ? Text(coorganizer.username[0]) : null,
-          ),
-        ),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => UserProfileViewPage(
-                viewedUsername: coorganizer.username,
-              ),
+          contentPadding: const EdgeInsets.all(16),
+          title: Text(
+            coorganizer.username,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+              color: Color(0xFF003675),
             ),
-          );
-        },
-        contentPadding: const EdgeInsets.all(16),
-        title: Text(
-          coorganizer.username,
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-            color: Color(0xFF003675),
           ),
-        ),
-        subtitle: Text(
-          coorganizer.email,
-          style: const TextStyle(color: Colors.black87),
+          subtitle: Text(
+            coorganizer.email,
+            style: const TextStyle(color: Colors.black87),
+          ),
         ),
       ),
     );
